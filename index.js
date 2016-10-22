2771286aa8c5ef6148aff422b82f6e2a5f6fe0a3
Lee, Seunghun <markandsh@gmail.com>
2016-10-22 15:50:54 -0400

    first working version of multi user chatapp

--- index.js
+++ index.js
@@ -1,19 +1,42 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var users= [], connectedUsers = [];
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+io.sockets.on('connection', function(socket){
+  connectedUsers.push(socket);
+  console.log('Connected: %s socket is connected', connectedUsers.length);
+
+  // Send Message
+  socket.on('send message', function(data){
+    io.sockets.emit('new message', {msg:data, username:socket.username});
   });
-  socket.on('disconnect', function() {
-  	console.log('user disconnected');
+
+  // Disconnect 
+  socket.on('disconnect', function(){
+  	  // if(!socket.username) return;
+  	  users.splice(users.indexOf(socket.username), 1);
+  	  updateUsernames();
+	  connectedUsers.splice(connectedUsers.indexOf(socket), 1);
+	  console.log('Disconnected: %s sockets are still connected', connectedUsers.length);
   });
+
+  // New User
+  socket.on('new user', function(data, callback){
+  	callback(true);
+  	socket.username = data;
+  	users.push(socket.username);
+  	updateUsernames();
+  });
+
+  function updateUsernames(){
+  	io.sockets.emit('get users', users);
+  }
+
 });
 
 http.listen(3000, function(){
